3d8f0a697a399763447b9e2f0006c69a92c14ebc
Rohan Koodli <rovik05@gmail.com>
2016-09-13 16:20:25 -0700

    Added graphs and elasticity data

--- ipc_microsim_tool/ipc_microsim_tool_sklearn_v2.py
+++ ipc_microsim_tool/ipc_microsim_tool_sklearn_v2.py
@@ -8,10 +8,13 @@ import pandas as pd
 import numpy as np
 
 # reading in aztlan data on income, kilowatt-hour per capita, and wgt
-aztlan = pd.read_csv('\Users\Rohan\Documents\GitHub\Electricity_Consumption_Surveys\ipc_microsim_tool\data\example_aztlan_seed.tab.txt',
+aztlan_seed = pd.read_csv('\Users\Rohan\Documents\GitHub\Electricity_Consumption_Surveys\ipc_microsim_tool\data\example_aztlan_seed.tab.txt',
                      sep='\t', index_col=None, na_values='')
-
-X = np.array(aztlan['income'])
+                     
+aztlan_elast = pd.read_csv('\Users\Rohan\Documents\GitHub\Electricity_Consumption_Surveys\ipc_microsim_tool\data\example_aztlan_w_elast.tab.txt',
+                     sep='\t', index_col=None, na_values='')
+'''
+X = np.array(aztlan_seed['income'])
 #print (X)
 #X = X[:, None]
 # X is all the income data values
@@ -20,24 +23,58 @@ for i in X:
     X1.append([i])
 
 #print X1
-y = np.array(aztlan['kwhpc'])
+y = list(aztlan_seed['kwhpc'])
+print type(y)
 # y is all the kilowatt-hour values
 print len(y)
 
-from sklearn import neighbors,tree,ensemble,svm
-rfr = ensemble.RandomForestRegressor()
-dtr = tree.DecisionTreeRegressor()
+X2 = np.array(aztlan_elast['income'])
+for j in X2:
+    X1.append([j])
+
+y2 = (aztlan_elast['kwhpc'])
+y.append(y2)
+print len(y)
+print len(X1)
+'''
+
+X1 = list(aztlan_seed['income'])
+X2 = list(aztlan_elast['income'])
+X = []
+for i in X1:
+    X.append([i])
+for j in X2:
+    X.append([j])
+
+print 'X length',len(X)
+
+y1 = list(aztlan_seed['kwhpc'])
+y2 = list(aztlan_elast['kwhpc'])
+y = []
+for k in y1:
+    y.append(k)
+for l in y2:
+    y.append(l)
+print 'y len',len(y)
+
+from sklearn import neighbors,svm#,tree,ensemble
+#rfr = ensemble.RandomForestRegressor()
+#dtr = tree.DecisionTreeRegressor()
 knr = neighbors.KNeighborsRegressor()
 svr = svm.SVR()
 #nnn = neural_network.BernoulliRBM()
 ''''''
 from sklearn.cross_validation import cross_val_score
-print cross_val_score(rfr,X1,y)
-print cross_val_score(dtr,X1,y)
-print cross_val_score(knr,X1,y)
-print cross_val_score(svr,X1,y)
+print 'KNeighbors Regressor', cross_val_score(knr,X,y,cv=2)
+print 'Support Vector Regressor', cross_val_score(svr,X,y,cv=2)
 ''''''
 from matplotlib import pyplot as plt
 import seaborn; seaborn.set()
 
-plt.scatter(X1,y)
+plt.scatter(X,y)
+plt.xlabel("Income")
+plt.ylabel('Kilowatt-hours per capita')
+plt.title('Aztlan Seed & Elasticity data')
+
+
+
